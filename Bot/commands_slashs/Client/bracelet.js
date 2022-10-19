13a6567393d3fa3632510c205f1944f3111aa389
Snowky92 <amaurypinheiro@hotmail.fr>
2022-10-19 13:21:48 +0200

    Update bracelet.js

--- Bot/commands_slashs/Client/bracelet.js
+++ Bot/commands_slashs/Client/bracelet.js
@@ -1,10 +1,10 @@
 module.exports = async (client,interaction)=>{
-	//Trouver le rôle
-
+	//Trouver le rôle 
+    let role = interaction.member.guild.roles.cache.get('1032221518249332746')
     //Trouver l'utilisateur
-    const User = interaction.options.getUser("utilisateur")
+    const member = interaction.options.getMember("utilisateur")
     //ajouter le rôle a l'utilisateur
-
+    member.roles.add(role)
 	await interaction.reply({ 
         content: "Le bracelet a bien été posé", 
         ephemeral: true
