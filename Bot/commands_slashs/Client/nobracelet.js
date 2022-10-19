9ed8c2cd474b8a8039f70d5292e0e4ff46bc5e6d
Snowky92 <amaurypinheiro@hotmail.fr>
2022-10-19 14:14:28 +0200

    update nobracelet

--- Bot/commands_slashs/Client/nobracelet.js
+++ Bot/commands_slashs/Client/nobracelet.js
@@ -2,7 +2,7 @@ module.exports = async (client,interaction)=>{
     //Trouver l'utilisateur
     const member = interaction.options.getMember("utilisateur")
     //retirer le rôle a l'utilisateur
-    member.roles.add('1032221518249332746')
+    member.roles.remove('1032221518249332746')
     //message de confirmation
 	await interaction.reply({ 
         content: "Le bracelet a bien été retiré", 
