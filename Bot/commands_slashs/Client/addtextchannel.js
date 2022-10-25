d26579f8f23b8c759c14f612a4a13a9a3126549a
Snowky92 <amaurypinheiro@hotmail.fr>
2022-10-25 13:23:49 +0200

    Ajout de la commande Addtextchannel

--- Bot/commands_slashs/Client/addtextchannel.js
+++ Bot/commands_slashs/Client/addtextchannel.js
@@ -1,12 +1,10 @@
 module.exports = async (client,interaction)=>{
     //Recup le nom du channel
-    const channel = interaction.options.get("channel")
+    const channel = interaction.options.getString("channel")
     //Recup l'id du server
     const guild = "1027139435680235550"
-    console.log(interaction);
- 
     //crée le channel
-    member.guild.channels.create({ name: {channel}})
+    interaction.member.guild.channels.create({name : channel})
 
 	await interaction.reply({ 
         content: "Le channel a bien été ajouter", 
