e855dc210ef8f0d67010e0e2651a61722eb45ecb
Liduen <raphael.orieux@gmail.com>
2022-10-26 16:08:31 +0200

    fenetres deplacables / resizables / minimizables

--- d3_front/testWindows/App.vue
+++ d3_front/testWindows/App.vue
@@ -1,67 +1,109 @@
 <script setup>
+import VueResizable from 'vue-resizable';
 import { ref } from "vue";
 
-let windows = {
-    "w-test": {
-        x: ref(20),
-        y: ref(200),
-        moving: ref(false)
+const dragSelector = ".w-picker";
+const handlers = ["r", "rb", "b", "lb", "l", "lt", "t", "rt"];
+const min = { w: 100, h: 100 };
+const fit = true;
+
+
+let activesWindows = ref([]);
+activesWindows.value.push({
+    name: "Firefox",
+    x: ref(50),
+    y: ref(200),
+    w: ref(300),
+    h: ref(200),
+    moving: ref(false),
+    fit: true,
+    icon: "Firefox_logo,_2019.svg",
+    minimized: false
+});
+
+activesWindows.value.push({
+    name : "Spotify",
+    x: ref(600),
+    y: ref(300),
+    w: ref(450),
+    h: ref(300),
+    moving: ref(false),
+    icon: "icon_spoty",
+    minimized: false
+});
+
+let minWindows = ref([
+    {
+        name: "Minecraft",
+        x: ref(600),
+        y: ref(300),
+        w: ref(450),
+        h: ref(300),
+        moving: ref(false),
+        icon: "minecraft-icon",
+        minimized: true
     }
+]);
+
+
+function log() {
+    // for (const fenetres in activesWindows) {
+    //     console.log(activesWindows[fenetres]);
+    // }
+
+    console.log(activesWindows);
+    console.log(minWindows);
 }
 
-let moving = ref(false);
-console.log(moving.value);
+function minimize(index) {
+    minWindows.value.push(activesWindows.value[index]);
+    activesWindows.value.splice(index, 1);
 
-function inMove(event, myWindow) {
-    // Inversion de la valeur moving
-    myWindow.moving.value = !myWindow.moving.value;
-
-    console.log(event);
-    // const startX = e.x;
-    // const startY = e.y;
-
-    window.addEventListener("mouseup", mouseUp);
-    window.addEventListener("mousemove", mouseMove);
-
-    function mouseMove(e) {
-        console.log(e);
-        // Application des coordonées
-        myWindow.x.value = e.x;
-        myWindow.y.value = e.y;
-    }
-
-
-    function mouseUp() {
-        // Inversion de la valeur moving
-        myWindow.moving.value = !myWindow.moving.value;
-
-        window.removeEventListener("mouseup", mouseUp);
-        window.removeEventListener("mousemove", mouseMove);
-    }
+    console.log(activesWindows);
 }
 
 </script>
 
 <template>
     <header>
-        <h1>Test fenètres déplaçables</h1>
+        <h1>Test fenêtres déplaçables</h1>
+        <button @click="log">Log</button>
     </header>
-    <div class="page" v-for="(window, index) of windows">
-        <h3>{{window.objectName}}</h3>
+    <div class="page" >
 
-        <div class="default" :class="[window.moving.value ? 'move' : 'static']" :id="index" :style="{top: window.y.value + 'px', left: window.x.value + 'px'}" >
-            <div class="w-picker" @mousedown="inMove(event, window)"></div>
-
+        <vue-resizable v-for="(window, index) of activesWindows" :ref="setWindows"
+        class="default"
+        :id="window.name"
+        :dragSelector="dragSelector"
+        :active="handlers"
+        :fit-parent="fit"
+        :width="window.w" :height="window.h"
+        :left="window.x" :top="window.y"
+        :min-width="min.w" :min-height="min.h">
+            <div class="w-picker">
+                <img :src="`./testWindows/assets/icons/${window.icon}.png`" alt="icon" class="iconW">
+                <div class="buttonBar">
+                    <button class="minimizeW" @click="minimize(index)">-</button>
+                    <button class="closeW">X</button>
+                </div>
+            </div>
+            
             <div class="w-content"></div>
-        </div>
+        </vue-resizable> 
 
+    </div>
+    <div class="toolbar">
+        <div class="minimContainer" v-for="(window, index) of minWindows">
+            <div class="minimApp" :id="index">
+                <img :src="`./testWindows/assets/icons/${window.icon}.png`" alt="icon">
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
-
 /* Mise en page */
-header{
+header {
     display: flex;
     justify-content: center;
     
@@ -75,39 +117,124 @@ header{
     flex-grow: 2;
     height: 90%;
     position: relative;
+    overflow: hidden;
+
+    background: no-repeat center url("./testWindows/assets/back/Squadron\ 42\ -\ Star\ Citizen\ Screenshot\ 2022.08.08\ -\ 18.14.29.28.png") ;
+}
+
+.resizable {
+    background-position: top left;
+    width: 150px;
+    height: 150px;
+    padding: 0;
+    border: 1px solid #003eff;
+    background: #007fff;
+    font-weight: normal;
+    color: #ffffff;
+    position: relative;
+}
+
+#Firefox {
+    z-index: 2;
 }
 
 /* Style Fenètres déplacables */
 .default {
     width: 25%;
     height: 300px;
-    border: 3px solid red;
-
+    outline: 1px solid black;
+    background-color: darkgoldenrod;
+    box-shadow: 2px 2px 4px black;
+    
     position: absolute;
     left: 20%;
     top: 50px;
-
+    
     display: flex;
     flex-direction: column;
     border-radius: 10px;
-
-    transition: border-color .2s;
+    
+    transition: border-color .2s, left .01s, top .01s, height .02s, width .02s; 
 }
 
 .w-picker {
-    height: 20px;
     background-color: gray;
     cursor: move;
-    border-radius: 5px;
+    border-radius: 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 30px;
+    padding: 3px;
 }
 
-/* Style Moving */
-
-.static {
-    border: 3px solid red;
+.toolbar {
+    height: 7%;
+    background-color: lightslategray;
 }
 
-.move {
-    border-color: cyan;
+.buttonBar {
+    display: flex;
+    justify-content: right;
+    align-items: center;
+    height: 100%;
+}
+
+.minimizeW, .closeW {
+    background-color: rgba(255, 255, 255, 0.474);
+    border-radius: 50%;
+    width: 1.8em;
+    height: 1.8em;
+    cursor: pointer;
+    border: 0;
+    margin-left: 5px;
+    transition: background-color .2s;
+}
+
+.minimizeW:hover {
+    background-color: rgba(255, 166, 0, 0.6);
+}
+
+.closeW:hover {
+    background-color: rgba(220, 20, 60, 0.8);
+}
+
+.iconW {
+    height: 80%;
+    padding-left: 3px;
+}
+
+
+/* Toolbar */
+
+.toolbar {
+    display: flex;
+    justify-content: center;
+}
+
+.minimContainer {
+    height: 100%;
+}
+
+
+.minimApp {
+    height: 92%;
+    display: flex;
+    align-items: center;
+    padding: 0 2vh;
+    margin-bottom: -4px;
+    background-color: rgba(255, 255, 255, 0.1);
+    border-bottom: 2px solid cyan;
+    border-radius: 3px;
+
+    transition: background-color .2s;
+}
+
+.minimApp:hover {
+    background-color: rgba(255, 255, 255, 0.3);
+}
+
+.minimApp img {
+    height: 60%;
 }
 </style>
