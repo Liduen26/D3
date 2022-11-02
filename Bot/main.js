c0c7f220bf88b7101cd55493da489d619c5494db
Ruben <85025254+Kaylua@users.noreply.github.com>
2022-11-02 18:32:53 +0100

    fusion des main

--- Bot/main.js
+++ Bot/main.js
@@ -117,3 +117,27 @@ const loadCycles = (dir = "./Cycle/") => {
 loadCycles();
 
 client.login(TOKEN);
+
+
+// API
+
+const express = require("express");
+const { connect } = require("../API/db/connect");
+const routerUtilisateurs = require("../API/routers/utilisateur");
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/", routerUtilisateurs);
+
+connect("mongodb://localhost:27017/", (err) => {
+  if (err) {
+    console.log("Erreur lors de la connexion à la base de données");
+    process.exit(-1);
+  } else {
+    console.log("Connexion avec la base de données établie");
+    app.listen(3000);
+    console.log("Attente des requêtes au port 3OOO");
+  }
+});
