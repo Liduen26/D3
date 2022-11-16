8408bd56e394edc8151fb5e063329cee2798b3c6
Ruben <85025254+Kaylua@users.noreply.github.com>
2022-11-16 16:01:50 +0100

    Améliorations+explications endpoints

--- API/routes/servers.js
+++ API/routes/servers.js
@@ -18,12 +18,20 @@ router.get('/:serverID', async (req, res) => {
     const serverID = req.params.serverID
     const guild = await discordinfo.client.guilds.fetch(serverID)
 
+    bot.on('interactionCreate', async interaction => {
+      const allMembers = await interaction.guild.members.fetch();
+      allMembers.forEach(m => {
+         console.log(m.user.username);
+      });
+     });
+
     // Get Members
     const members = await guild.members.cache.map(member => ({id: member.id, idtype: "memberID", name: member.displayName}))
 
     // Get Channels
     const channels = await guild.channels.cache.map(channel => ({id: channel.id, idtype: "channelID", name: channel.name, channeltype: channel.type}))
-    const channelsModified = channels.filter(channel => channel.channeltype === "text")
+    const channelsModified = channels.filter(channel => channel.channeltype)
+    // const channelsModified = channels.filter(channel => channel.channeltype === "text")
 
     // Create Response
     const response = {members: members, channels: channelsModified}
